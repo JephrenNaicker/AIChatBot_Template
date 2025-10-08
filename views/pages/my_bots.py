65c2adc49d71facb510afc969e75797ba9551027
Jephren Naicker <54954891+JephrenNaicker@users.noreply.github.com>
2025-10-08 14:35:11 +0200

    Partly working delete

--- views/pages/my_bots.py
+++ views/pages/my_bots.py
@@ -4,8 +4,19 @@ from controllers.bot_manager_controller import BotManager
 from components.bot_card_manage import manage_bot_card
 from components.bot_card import get_bot_card_css
 
+
 async def my_bots_page():
     """Main entry point for the My Bots page"""
+    # Handle pending bot actions first
+    if 'pending_bot_action' in st.session_state:
+        action = st.session_state.pending_bot_action
+        if action["type"] == "delete":
+            await BotManager._delete_bot(action["bot_name"])
+        elif action["type"] == "update_status":
+            await BotManager._update_bot_status(action["bot_name"], action["new_status"])
+        # Clear the pending action
+        del st.session_state.pending_bot_action
+
     # Inject CSS
     st.markdown(get_bot_card_css(), unsafe_allow_html=True)
 
